2d19c9955443442838a5eefb6ed59ed2138473f0
Lucas Dimer Justo <lucas.dimer.justo@hotmail.com>
2021-07-25 17:26:51 -0300

    getting user ID and getting user permission to be recognized from iCloud.

--- MatchMaker/MatchMaker/CKRepository.swift
+++ MatchMaker/MatchMaker/CKRepository.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import CloudKit
+import UIKit
+
+public class CKRepository {
+    static var user: User? //singleton user
+    private static let container: CKContainer = CKContainer(identifier: "iCloud.MatchMaker")
+    
+    public static func setOnboardingInfo(name: String, nickname: String, photo: UIImage?, country: String, description: String, languages: [String]){
+        //asks iCloud Permission
+        let permission = iCloudPermission()
+        
+        
+        if permission {
+            //getUserId
+            let id = getUserId()
+            user = User(id: id, name: name, nickname: nickname, photo: photo ?? nil, country: country, description: description, behaviourRate: 0, skillRate: 0, languages: languages)
+        }
+    }
+    
+    private static func iCloudPermission() -> Bool {
+        var hasICloudPermission = false
+        DispatchQueue.global().sync {
+            container.requestApplicationPermission(.userDiscoverability) { status, error in
+                let cloudError = error as? CKError
+                switch cloudError?.code {
+                    case .notAuthenticated: print("dale")
+                    default: print("nao dale")
+                }
+                hasICloudPermission = status == .granted
+            }
+        }
+        return hasICloudPermission
+    }
+    
+    private static func getUserId() -> String{
+        var id: String = ""
+        DispatchQueue.global().sync {
+            container.fetchUserRecordID { record, error in
+                id = record?.recordName ?? ""
+            }
+        }
+        return id
+    }
+    
+    private static func storeUserData(id: String, name: String, nickname: String, country: String, description: String, photo: UIImage?, selectedPlatforms: [String], selectedGames: [Game]){
+        
+    }
+}
--- MatchMaker/MatchMaker/UserModel.swift
+++ MatchMaker/MatchMaker/UserModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import UIKit
+
+public struct Social {
+    let id: String //iCloud ID
+    let name: String //real name
+    let nickname: String //in game name
+    let photo: UIImage //profile picture
+}
+
+public class User {
+    let id: String //iCloud ID
+    var name: String //real name
+    var nickname: String //in game name
+    var photo: UIImage? //profile picture
+    var country: String //where the user is playing from
+    var description: String
+    var behaviourRate: Double
+    var skillRate: Double
+    //var selectedPlatforms: [Platforms]
+    var languages: [String]
+    //var selectedGames: [Game]
+    //var friends: [Social]
+    //var blocked: [Social]
+    
+    init(id: String, name: String, nickname: String, photo: UIImage?, country: String, description: String, behaviourRate: Double, skillRate: Double,languages: [String]){
+        self.id = id
+        self.name = name
+        self.nickname = nickname
+        self.photo = photo ?? nil
+        self.country = country
+        self.description = description
+        self.behaviourRate = behaviourRate
+        self.skillRate = skillRate
+        self.languages = languages
+    }
+}
