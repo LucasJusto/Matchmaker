561c9481de9618b2fef6dbc3913728598377e0e0
João Brentano <joaobrentano.br@gmail.com>
2021-07-26 16:24:36 -0300

    GameDetail images

---
+++ MatchMaker/MatchMaker/Views/GameDetail/GameDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameDetailViewController.swift
+//  MatchMaker
+//
+//  Created by João Brentano on 25/07/21.
+//
+
+import UIKit
+
+class GameDetailViewController: UIViewController {
+    
+    @IBOutlet weak var GameImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
