654c4c8a41bb3614db55983b930cf55b59efb3c3
Marina Moreira <38846018+MarinaFX@users.noreply.github.com>
2021-07-26 19:57:22 -0300

    chore: created profile structure + custom profile rating xib

--- MatchMaker/MatchMaker/ViewController.swift
+++ MatchMaker/MatchMaker/ViewController.swift
@@ -9,20 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var label: UILabel!
+    //@IBOutlet weak var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        label.setLabelText(for: "titleLabel", comment: "Title label example")
+        //label.setLabelText(for: "titleLabel", comment: "Title label example")
     }
 }
 
 extension UILabel {
     
-    func setLabelText(for key: String, comment: String) {
-        self.text = NSLocalizedString(key, comment: comment)
-    }
+//    func setLabelText(for key: String, comment: String) {
+//        self.text = NSLocalizedString(key, comment: comment)
+//    }
     
 }
