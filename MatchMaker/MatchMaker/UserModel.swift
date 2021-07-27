136953aff6c0f187d2041cc137307e8440111b47
Lucas Dimer Justo <lucas.dimer.justo@hotmail.com>
2021-07-27 18:35:09 -0300

    addding languages enum to see which languages each user will set at their profile.

--- MatchMaker/MatchMaker/UserModel.swift
+++ MatchMaker/MatchMaker/UserModel.swift
@@ -8,6 +8,69 @@
 import Foundation
 import UIKit
 
+enum Languages: CustomStringConvertible {
+    case portuguese, english, russian, chinese, japanese, german, spanish
+    
+    var description: String {
+        switch self {
+            case .portuguese:
+                return NSLocalizedString("LanguagesPortuguese", comment: "portuguese")
+            case .english:
+                return NSLocalizedString("LanguagesEnglish", comment: "english")
+            case .russian:
+                return NSLocalizedString("LanguagesRussian", comment: "russian")
+            case .chinese:
+                return NSLocalizedString("LanguagesChinese", comment: "chinese")
+            case .japanese:
+                return NSLocalizedString("LanguagesJapanese", comment: "japanese")
+            case .german:
+                return NSLocalizedString("LanguagesGerman", comment: "german")
+            case .spanish:
+                return NSLocalizedString("LanguagesSpanish", comment: "spanish")
+        }
+    }
+    
+    var key: String {
+        switch self {
+            case .portuguese:
+                return "LanguagesPortuguese"
+            case .english:
+                return "LanguagesEnglish"
+            case .russian:
+                return "LanguagesRussian"
+            case .chinese:
+                return "LanguagesChinese"
+            case .japanese:
+                return "LanguagesJapanese"
+            case .german:
+                return "LanguagesGerman"
+            case .spanish:
+                return "LanguagesSpanish"
+        }
+    }
+    
+    static func getLanguage(language: String) -> Languages {
+        switch language {
+            case "LanguagesPortuguese":
+                return Languages.portuguese
+            case "LanguagesEnglish":
+                return Languages.english
+            case "LanguagesRussian":
+                return Languages.russian
+            case "LanguagesChinese":
+                return Languages.chinese
+            case "LanguagesJapanese":
+                return Languages.japanese
+            case "LanguagesGerman":
+                return Languages.german
+            case "LanguagesSpanish":
+                return Languages.spanish
+            default:
+                return Languages.english
+        }
+    }
+}
+
 public struct Social {
     let id: String //iCloud ID
     let name: String //real name
@@ -25,12 +88,12 @@ public class User {
     var behaviourRate: Double
     var skillRate: Double
     var selectedPlatforms: [Platform]
-    var languages: [String]
+    var languages: [Languages]
     var selectedGames: [Game]
     var friends: [Social]
     var blocked: [Social]
     
-    init(id: String, name: String, nickname: String, photo: UIImage?, country: String, description: String, behaviourRate: Double, skillRate: Double,languages: [String], selectedPlatforms: [Platform], selectedGames: [Game]){
+    init(id: String, name: String, nickname: String, photo: UIImage?, country: String, description: String, behaviourRate: Double, skillRate: Double,languages: [Languages], selectedPlatforms: [Platform], selectedGames: [Game]){
         self.id = id
         self.name = name
         self.nickname = nickname
