ebdd08913c9d11c23d988d9bdbf0842b4f27dfc7
João Brentano <joaobrentano.br@gmail.com>
2021-07-27 17:37:14 -0300

    Started populating Game Detail screen with data

--- MatchMaker/MatchMaker/Views/GameDetail/GameDetailViewController.swift
+++ MatchMaker/MatchMaker/Views/GameDetail/GameDetailViewController.swift
@@ -9,12 +9,39 @@ import UIKit
 
 class GameDetailViewController: UIViewController {
     
+    // Game mock
+    let game = (Games.buildGameArray()[1])
+    
+    // Game info
     @IBOutlet weak var GameImage: UIImageView!
+    @IBOutlet weak var GameTitle: UILabel!
+    @IBOutlet weak var GameDescription: UILabel!
+    
+    // Platform images
+    @IBOutlet weak var PlaystationImage: UIImageView!
+    @IBOutlet weak var XboxImage: UIImageView!
+    @IBOutlet weak var PCImage: UIImageView!
+    @IBOutlet weak var MobileImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        // Game info
+        GameImage.image = game.image
+        GameTitle.text = game.name
+        GameDescription.text = game.description
+        for platform in game.platforms {
+            switch(platform) {
+            case .PC:
+                PCImage.image = UIImage(named: "PC_selected")
+            case .PlayStation:
+                PlaystationImage.image = UIImage(named: "Play_selected")
+            case .Xbox:
+                XboxImage.image = UIImage(named: "Xbox_selected")
+            case .Mobile:
+                MobileImage.image = UIImage(named: "Mobile_selected")
+            }
+        }
     }
     
 
