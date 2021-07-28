dbcacf8b04bd290f3e19cb766c1bd4c06ead9850
Marina Moreira <38846018+MarinaFX@users.noreply.github.com>
2021-07-28 18:34:29 -0300

    chore: finished xib frontend + missing imagePicker feat

---
+++ MatchMaker/MatchMaker/Views/Xibs/UserAvatar/UserAvatarView.swift
@@ -0,0 +1,38 @@
+//
+//  UserAvatarView.swift
+//  MatchMaker
+//
+//  Created by Marina De Pazzi on 27/07/21.
+//
+
+import UIKit
+
+@IBDesignable class UserAvatarView: UIView, NibLoadable {
+
+    @IBOutlet weak var contentImage: UIImageView!
+    @IBOutlet weak var buttonBackground: UIImageView!
+    @IBOutlet weak var photoButton: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        contentImage.layer.masksToBounds = true
+        contentImage.layer.cornerRadius = 60
+        contentImage.layer.cornerCurve = .circular
+        
+        buttonBackground.layer.masksToBounds = true
+        buttonBackground.layer.cornerRadius = 13
+        buttonBackground.layer.cornerCurve = .circular
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupFromNib()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupFromNib()
+    }
+
+}
