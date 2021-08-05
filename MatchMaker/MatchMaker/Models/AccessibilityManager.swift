70044c0e247a2ced6633aa865ae4ae82cc3bdd71
Marina Moreira <38846018+MarinaFX@users.noreply.github.com>
2021-08-05 13:57:12 -0300

    docs: fixing readmes

--- MatchMaker/MatchMaker/Models/AccessibilityManager.swift
+++ MatchMaker/MatchMaker/Models/AccessibilityManager.swift
@@ -9,23 +9,23 @@ import Foundation
 import UIKit
 
 //MARK: - AccessibilityManager Class
-
+/**
+ AccessibilityManager is a class created to abstract and facilitate the process of implementing DynamicTypes to our project. Instead manually creating UIFontMetrics and Scalable fonts, this class abstracts this setup and returns a scalable font.
+ */
 class AccessibilityManager {
     
     /**
      Sets up a custom font when enabling DynamicTypes
      
      - Parameters:
-        - textStyle: Style of the font to be scaled | *UIFont.TextStyle*
-        - fontSize: Size of the font to be scaled | *CGFloat*
-        - fontWeight: The Weight of font to be scaled | *UIFont.Weight*
+        - textStyle: Style of the font to be scaled
+        - fontSize: Size of the font to be scaled
+        - fontWeight: The Weight of font to be scaled
      
      - returns: the scaled font based on the given params
      */
     static func forCustomFont(forTextStyle textStyle: UIFont.TextStyle, forFontSize fontSize: CGFloat, forFontWeight fontWeight: UIFont.Weight) -> UIFont {
-    
         let fontMetrics = UIFontMetrics.init(forTextStyle: textStyle)
-        
         let font = UIFont.systemFont(ofSize: fontSize, weight: fontWeight)
         
         return fontMetrics.scaledFont(for: font)
