348c744a9f47adc90085956ed50ae36824cb6123
João Brentano <joaobrentano.br@gmail.com>
2021-08-10 17:00:35 -0300

    Added XIB

--- MatchMaker/MatchMaker/Views/GameDetail/GameDetailViewController.swift
+++ MatchMaker/MatchMaker/Views/GameDetail/GameDetailViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class GameDetailViewController: UIViewController {
     
     // Game mock
-    let game = Games.games[1]
+    var game = Games.games[1]
     
     // Game info
     @IBOutlet weak var GameImage: UIImageView!
@@ -31,8 +31,14 @@ class GameDetailViewController: UIViewController {
     @IBOutlet weak var PlatformsLabel: UILabel!
     @IBOutlet weak var OtherGamesLabel: UILabel!
     
+    // Xib
+    @IBOutlet weak var OtherGamesXib: RoundedRectangleCollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Xib setup
+        OtherGamesXib.RoundedRectangleImageModels = Games.games
         
         // Label setup
         PlatformsLabel.text = NSLocalizedString("PlatformsLabel", comment: "Platforms label")
@@ -43,10 +49,13 @@ class GameDetailViewController: UIViewController {
         AddToMyGamesButton.cornerRadius = 10
         FindPlayersButton.setTitle(NSLocalizedString("FindPlayersButton", comment: "Button on GameDetailView for searching players to play with"), for: UIControl.State.init())
         AddToMyGamesButton.setTitle(NSLocalizedString("AddToMyGamesButton", comment: "Button on GameDetailView to add a game to the user's games"), for: UIControl.State.init())
+        
         // Game info
         GameImage.image = game.image
         GameTitle.text = game.name
         GameDescription.text = game.description
+        
+        //Platforms setup
         for platform in game.platforms {
             switch(platform) {
                 case .PC:
