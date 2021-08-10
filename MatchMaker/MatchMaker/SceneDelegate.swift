f4eef67a6afa1216c19dacec7cd9009055daa3dd
João Brentano <joaobrentano.br@gmail.com>
2021-08-10 17:24:08 -0300

    Forgot to uncomment SceneDelegate, done

--- MatchMaker/MatchMaker/SceneDelegate.swift
+++ MatchMaker/MatchMaker/SceneDelegate.swift
@@ -17,16 +17,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             if bool {
                 CKRepository.setUserFromCloudKit()
             }
-//            else {
-//                DispatchQueue.main.async {
-//                    let storyboard = UIStoryboard.init(name: "Onboarding", bundle: nil)
-//
-//                    let viewController = storyboard.instantiateViewController(withIdentifier: "OnboardingViewController") as! OnboardingViewController
-//
-//                    self.window?.rootViewController = viewController
-//                    self.window?.makeKeyAndVisible()
-//                }
-//            }
+            else {
+                DispatchQueue.main.async {
+                    let storyboard = UIStoryboard.init(name: "Onboarding", bundle: nil)
+
+                    let viewController = storyboard.instantiateViewController(withIdentifier: "OnboardingViewController") as! OnboardingViewController
+
+                    self.window?.rootViewController = viewController
+                    self.window?.makeKeyAndVisible()
+                }
+            }
         }
     }
 
