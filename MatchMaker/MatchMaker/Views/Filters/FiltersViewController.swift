f2449164abcb68a2cd292f37d836299901d022d9
Thaís Fernandes <throdriguesfernandes@gmail.com>
2021-08-12 17:42:08 -0300

    added languages and platforms sections and created rate cell on storyboard

--- MatchMaker/MatchMaker/Views/Filters/FiltersViewController.swift
+++ MatchMaker/MatchMaker/Views/Filters/FiltersViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class FiltersViewController: UIViewController {
-
+    
     //MARK: - Typealias
     typealias TagOption = (option: String, isFavorite: Bool)
     typealias GameOption = (option: Game, isFavorite: Bool)
     typealias UserLocation = (string: String, enum: Locations)
-
+    
     
     //MARK: - Outlets
     @IBOutlet weak var tableView: UITableView!
@@ -48,6 +48,12 @@ class FiltersViewController: UIViewController {
         }
     }
     
+    enum TagFilterOptions: Int, CaseIterable {
+        case languages
+        case platforms
+        case games
+    }
+    
     //MARK: - Functions
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,28 +62,22 @@ class FiltersViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        loadData()
     }
     
     func loadData() {
-        let needsLoading = [FilterOptions.languages, FilterOptions.platforms, FilterOptions.games]
-        
-        needsLoading.forEach { filter in
+        TagFilterOptions.allCases.forEach { filter in
             switch filter {
                 case .languages:
                     self.tagLanguages = Languages.allCases.map { TagOption(option: $0.description, isFavorite: false) }
-
+                    
                 case .platforms:
                     self.tagPlatforms = Platform.allCases.map { TagOption(option: $0.description, isFavorite: false) }
-                
+                    
                 case .games:
                     let games = Games.buildGameArray()
                     self.selectedGames = games.map { GameOption(option: $0, isFavorite: false) }
-//                case .behaviourRate:
-//                    <#code#>
-//                case .skillsRate:
-//                    <#code#>
-//                case .location:
-//                    <#code#>
             }
         }
     }
@@ -92,10 +92,11 @@ class FiltersViewController: UIViewController {
     }
 }
 
+// MARK: - UITableViewDataSource
 extension FiltersViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     
@@ -105,9 +106,137 @@ extension FiltersViewController: UITableViewDataSource {
         
         let filter = FilterOptions.allCases[indexPath.row]
         
-        return defaultCell
+        switch filter {
+            case .languages: return selectorCell(title: filter.description, tag: TagFilterOptions.languages.rawValue) ?? defaultCell
+            case .platforms: return selectorCell(title: filter.description, tag: TagFilterOptions.platforms.rawValue) ?? defaultCell
+            case .behaviourRate:
+                return defaultCell
+            case .skillsRate:
+                return defaultCell
+            case .location:
+                return defaultCell
+            case .games:
+                return defaultCell
+        }
+    }
+    
+    func selectorCell(title: String, tag: Int) -> SelectorTableViewCell? {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "selector-cell") as? SelectorTableViewCell
         
+        cell?.setUp(title: title)
+        
+        cell?.collectionView.tag = tag
+        cell?.collectionView.delegate = self
+        cell?.collectionView.dataSource = self
+        
+        return cell
     }
 }
 
+// MARK: - UITableViewDelegate
 extension FiltersViewController: UITableViewDelegate {}
+
+// MARK: - UICollectionViewDataSource
+extension FiltersViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        guard let selectedTagFilter = TagFilterOptions(rawValue: collectionView.tag) else {
+            return 0
+        }
+        
+        switch selectedTagFilter {
+            case .languages: return tagLanguages.count
+            case .platforms: return tagPlatforms.count
+            case .games: return selectedGames.count
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var collectionCell = SelectableTagCollectionViewCell()
+        
+        guard let selectedTagFilter = TagFilterOptions(rawValue: collectionView.tag) else {
+            return UICollectionViewCell()
+        }
+        
+        if selectedTagFilter == .games {
+            return collectionCell
+            
+        } else {
+            
+            collectionCell = collectionView.dequeueReusableCell(withReuseIdentifier: "selectable-tag-cell", for: indexPath) as! SelectableTagCollectionViewCell
+            
+            var tagOption: TagOption?
+            
+            switch selectedTagFilter {
+                case .languages: tagOption = tagLanguages[indexPath.row]
+                case .platforms: tagOption = tagPlatforms[indexPath.row]
+                case .games: break
+            }
+            
+            if let tagOption = tagOption {
+                collectionCell.labelView.text = tagOption.option
+                collectionCell.containerView.backgroundColor = tagOption.isFavorite ? UIColor(named: "Primary") : .clear
+            }
+            
+            return collectionCell
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+        guard let selectedTagFilter = TagFilterOptions(rawValue: collectionView.tag) else {
+            return
+        }
+
+        switch selectedTagFilter {
+            case .languages:
+                if tagLanguages.count != 1 {
+                    tagLanguages[indexPath.row].isFavorite.toggle()
+                }
+            case .platforms:
+                if tagPlatforms.count != 1 {
+                    tagPlatforms[indexPath.row].isFavorite.toggle()
+                }
+            case .games:
+                return
+        }
+
+        collectionView.reloadItems(at: [indexPath])
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension FiltersViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        guard let selectedTagFilter = TagFilterOptions(rawValue: collectionView.tag) else {
+            return CGSize(width: 0, height: 0)
+        }
+
+        var model: String
+
+        switch selectedTagFilter {
+            case .languages:
+                model = tagLanguages[indexPath.row].option
+            case .platforms:
+                model = tagPlatforms[indexPath.row].option
+            case .games:
+                return CGSize(width: 0, height: 0)
+        }
+
+        let modelSize = model.size(withAttributes: nil)
+
+        let size = CGSize(width: modelSize.width, height: collectionView.bounds.height)
+
+        return size
+    }
+
+}
+
+// MARK: - UICollectionViewDelegate
+extension FiltersViewController: UICollectionViewDelegate {}
