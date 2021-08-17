de84c3eb4f5df1cd84f4a45991359518c5929796
João Brentano <joaobrentano.br@gmail.com>
2021-08-16 21:02:02 -0300

    Undone temp changes for dev

--- MatchMaker/MatchMaker/SceneDelegate.swift
+++ MatchMaker/MatchMaker/SceneDelegate.swift
@@ -18,17 +18,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             if bool {
                 CKRepository.setUserFromCloudKit()
             }
-//            else {
-//                DispatchQueue.main.async {
-//                    let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
-//
-//                    let viewController = storyboard.instantiateInitialViewController()!
-//                    //let viewController = storyboard.instantiateViewController(withIdentifier: "OnboardingViewController") as! OnboardingViewController
-//
-//                    self.window?.rootViewController = viewController
-//                    self.window?.makeKeyAndVisible()
-//                }
-//            }
+            else {
+                DispatchQueue.main.async {
+                    let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+
+                    let viewController = storyboard.instantiateInitialViewController()!
+                    //let viewController = storyboard.instantiateViewController(withIdentifier: "OnboardingViewController") as! OnboardingViewController
+
+                    self.window?.rootViewController = viewController
+                    self.window?.makeKeyAndVisible()
+                }
+            }
         }
     }
 
