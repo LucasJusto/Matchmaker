8d2205caaf23cac43c1ad050ac79b3cc921a3271
Marina Moreira <38846018+MarinaFX@users.noreply.github.com>
2021-08-18 13:57:09 -0300

    fix: final fix on layout issue

--- MatchMaker/MatchMaker/Views/Profile/ProfileViewController.swift
+++ MatchMaker/MatchMaker/Views/Profile/ProfileViewController.swift
@@ -13,6 +13,8 @@ class ProfileViewController: UIViewController {
     
     //MARK: ProfileViewController - Variables and Outlets Setup
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     @IBOutlet weak var backgroundCoverImage: UIImageView!
     
     @IBOutlet weak var userAvatarView: UserAvatarView!
@@ -32,6 +34,10 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var languagesView: TitleCollectionView!
     @IBOutlet weak var gameCollectionView: RoundedRectangleCollectionView!
     
+    @IBOutlet weak var backgroundImageTopConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var backgroundImageHeightConstraint: NSLayoutConstraint!
+    
     var customPicker: ImagePickerManager = ImagePickerManager()
     
     private var user: User?
@@ -44,6 +50,7 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
         
         userAvatarView.delegate = self
+        scrollView.delegate = self
             
         //2 maneira de capturar a imagem do usuario, mas usando closures
 //        userAvatarView.didChooseImage = { [weak self] in
@@ -147,5 +154,14 @@ extension ProfileViewController: UserAvatarViewDelegate {
                 self.userAvatarView.contentImage.contentMode = .scaleAspectFill
             }
         }
+    }
+}
+//MARK: - UIScrollViewDelegate
+
+extension ProfileViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        backgroundImageHeightConstraint.constant = 242 - 48 + (-1 * scrollView.contentOffset.y)
+        view.layoutIfNeeded()
     }
 }
